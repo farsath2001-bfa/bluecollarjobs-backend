3cf54116aecadfd14067a9f1a2a5a8e3fd097fe2
Farsath <farsath2001@gmail.com>
2026-06-21 13:54:41 +0400

    Allow all origins for CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
